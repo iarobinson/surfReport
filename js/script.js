a4b381692a5e36d40fc4b1860015bd3f7d592b1e
Ian <ian (at) ianrobinson.net>
2018-02-24 17:38:57 -0500

    Add a potential fix for CORS problem

--- js/script.js
+++ js/script.js
@@ -9,9 +9,41 @@ $(document).ready(function () {
   console.log(urlConstructed);
   
   // Get info from API call
-  $.getJSON(urlConstructed, function(data) {
-    console.log(data);
-    $('.data').append(data);
-  });
+  // $.getJSON(urlConstructed, function(data) {
+  //   console.log(data);
+  //   $('.data').append(data);
+  // });
+  
+  //Get info from API call
+  var xhr = createCORSRequest('GET', urlConstructed);
+  console.log(xhr);
+  if (!xhr) {
+    throw new Error('CORS not supported');
+  }
 });
 
+
+// Thank you to https://www.html5rocks.com/en/tutorials/cors/ Monsur Hossain for this
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
